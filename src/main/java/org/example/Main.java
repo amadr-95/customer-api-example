4b26d43d0690b2963fc4761b7a12fc8cda906c76
Amador <amasabcar@outlook.com>
2024-05-01 20:55:21 +0200

    add code to see the beans instantiated by spring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,20 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-        /*CustomerDAO db = new CustomerDAO();
-        System.out.println(db.getCustomers());*/
+        ConfigurableApplicationContext applicationContext =
+                SpringApplication.run(Main.class, args);
+
+        //Beans in Application context
+        /*String[] beansNames = applicationContext.getBeanDefinitionNames();
+        for (String beanName : beansNames) {
+            System.out.println(beanName);
+        }*/
+
     }
 }
