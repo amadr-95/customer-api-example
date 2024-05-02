824f4e5d573adfa6d54f1a09419f1322fde211d6
Amador <amasabcar@outlook.com>
2024-05-02 17:36:53 +0200

    remove @Query annotation

--- src/main/java/org/example/customer/CustomerRepository.java
+++ src/main/java/org/example/customer/CustomerRepository.java
@@ -1,10 +1,7 @@
 package org.example.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    @Query(value = "SELECT COUNT(*) FROM customer c WHERE c.email = :email", nativeQuery = true)
-    boolean existsCustomerByEmail(@Param("email") String email);
+    boolean existsCustomerByEmail(String email);
 }
