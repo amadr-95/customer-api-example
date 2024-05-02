01f7acf064d09b622d1bcab00e8d6037061c2b8c
Amador <amasabcar@outlook.com>
2024-05-02 10:02:54 +0200

    add column constraints

--- src/main/java/org/example/customer/Customer.java
+++ src/main/java/org/example/customer/Customer.java
@@ -18,8 +18,12 @@ public class Customer {
             generator = "customer_id_sequence"
     )
     private Integer id;
+
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private LocalDate birth;
 
     public Customer() {}
